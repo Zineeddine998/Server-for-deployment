00edfc4654a5e182b1ad49289a2d8610d4aa8388
Zineeddine998 <55639522+Zineeddine998@users.noreply.github.com>
2020-05-26 19:31:34 +0100

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -7,10 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- 
-res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
-
+  res.header("Access-Control-Allow-Headers", "Accept, Content-Type");
   next();
 });
 const port = process.env.PORT || 3000;
